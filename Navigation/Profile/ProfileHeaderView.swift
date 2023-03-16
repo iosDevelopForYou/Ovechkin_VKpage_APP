2791818536576dd88327f3f0c4ef2d275b2be275
iosDevelopForYou <iosdevelopforyou@gmail.com>
2023-03-16 20:31:38 +0400

    user image view animation was added

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -55,15 +55,38 @@ class ProfileHeaderView: UIView {
         return textField
     }()
     
+    private let subAvatarView: UIView = {
+        let subImageView = UIView()
+        subImageView.translatesAutoresizingMaskIntoConstraints = false
+        subImageView.backgroundColor = .black
+        subImageView.alpha = 0.3
+        subImageView.frame = UIScreen.main.bounds
+        return subImageView
+    }()
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(closeAction), for: .touchUpInside)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
-        
+        setupGesture()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private var topUserImage = NSLayoutConstraint()
+    private var widthUserImage = NSLayoutConstraint()
+    private var leadingUserImage = NSLayoutConstraint()
+    private var heightUserImage = NSLayoutConstraint()
     
     func setupView() {
         addSubview(statusTextField)
@@ -71,16 +94,18 @@ class ProfileHeaderView: UIView {
         addSubview(profileNameLabel)
         addSubview(statusShowButton)
         
+        topUserImage = userImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16)
+        leadingUserImage = userImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        widthUserImage = userImageView.widthAnchor.constraint(equalToConstant: 130)
+        heightUserImage = userImageView.heightAnchor.constraint(equalToConstant: 130)
+        
         NSLayoutConstraint.activate([
-            userImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
-            userImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            userImageView.widthAnchor.constraint(equalToConstant: 130),
-            userImageView.heightAnchor.constraint(equalToConstant: 130),
+            topUserImage, leadingUserImage, widthUserImage, heightUserImage,
             
             profileNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
             profileNameLabel.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 25),
             
-            statusShowButton.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 16),
+            statusShowButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 160),
             statusShowButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             statusShowButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             statusShowButton.heightAnchor.constraint(equalToConstant: 50),
@@ -89,6 +114,54 @@ class ProfileHeaderView: UIView {
             statusTextField.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 25),
         ])
         
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAction))
+        userImageView.isUserInteractionEnabled = true
+        userImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func tapAction() {
+        addSubview(subAvatarView)
+        addSubview(closeButton)
+        bringSubviewToFront(userImageView)
+        
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            closeButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+            self.subAvatarView.alpha = 0.8
+            self.userImageView.layer.cornerRadius = 0
+            self.topUserImage.constant = 100
+            self.leadingUserImage.constant = 0
+            self.widthUserImage.constant = UIScreen.main.bounds.width
+            self.heightUserImage.constant = UIScreen.main.bounds.width
+            self.layoutIfNeeded()
+            
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3) {
+                self.closeButton.alpha = 1
+            }
+        }
+    }
+    
+    @objc private func closeAction() {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            self.closeButton.alpha = 0
+        } completion: { _ in
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
+                self.userImageView.layer.cornerRadius = 65
+                self.topUserImage.constant = 16
+                self.leadingUserImage.constant = 16
+                self.widthUserImage.constant = 130
+                self.heightUserImage.constant = 130
+                self.subAvatarView.alpha = 0
+                self.layoutIfNeeded()
+            }
+        }
     }
     
     @objc func buttonPressed() {
